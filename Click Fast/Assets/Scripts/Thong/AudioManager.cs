1bc0cfc93f5bc027baddd3244c513a9abda30f6e
ThMii <169111344+ThMii@users.noreply.github.com>
2025-02-12 16:40:52 +0700

    ưeqeqweqwe

---
+++ Click Fast/Assets/Scripts/Thong/AudioManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+
+    [Header("Audio Clips")]
+    public AudioClip backgroundMusic;
+    public AudioClip correctSound;
+    public AudioClip wrongSound;
+    public AudioClip buttonClickSound;
+
+    private AudioSource musicSource;
+    private AudioSource sfxSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        musicSource = gameObject.AddComponent<AudioSource>();
+        sfxSource = gameObject.AddComponent<AudioSource>();
+
+        musicSource.loop = true;
+    }
+
+    public void PlayBackgroundMusic()
+    {
+        if (backgroundMusic != null)
+        {
+            musicSource.clip = backgroundMusic;
+            musicSource.Play();
+        }
+    }
+
+    public void PlaySoundEffect(string soundType)
+    {
+        switch (soundType)
+        {
+            case "correct":
+                sfxSource.PlayOneShot(correctSound);
+                break;
+            case "wrong":
+                sfxSource.PlayOneShot(wrongSound);
+                break;
+            case "click":
+                sfxSource.PlayOneShot(buttonClickSound);
+                break;
+            default:
+                Debug.LogWarning("Âm thanh không hợp lệ: " + soundType);
+                break;
+        }
+    }
+
+    public void StopBackgroundMusic()
+    {
+        musicSource.Stop();
+    }
+}
