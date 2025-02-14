b4c409102daa60bfbff31a941ead0cf8cd7afd48
ThMii <169111344+ThMii@users.noreply.github.com>
2025-02-14 13:27:29 +0700

    â

--- Click Fast/Assets/Scripts/Thuan/VolumeController.cs
+++ Click Fast/Assets/Scripts/Thuan/VolumeController.cs
@@ -4,28 +4,25 @@ using UnityEngine.Audio;
 
 public class VolumeController : MonoBehaviour
 {
-    [SerializeField] private AudioMixer audioMixer; // Gán AudioMixer vào đây
+    [SerializeField] private AudioMixer audioMixer; 
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
 
     private void Start()
     {
-        // Load giá trị đã lưu trước đó (nếu có)
         musicSlider.value = PlayerPrefs.GetFloat("musicVolume", 1f);
         sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume", 1f);
 
-        // Gán sự kiện thay đổi giá trị cho Slider
         musicSlider.onValueChanged.AddListener(SetMusicVolume);
         sfxSlider.onValueChanged.AddListener(SetSFXVolume);
 
-        // Thiết lập âm lượng ban đầu
         SetMusicVolume(musicSlider.value);
         SetSFXVolume(sfxSlider.value);
     }
 
     public void SetMusicVolume(float volume)
     {
-        volume = Mathf.Clamp(volume, 0.0001f, 1f); // Tránh log10(0)
+        volume = Mathf.Clamp(volume, 0.0001f, 1f); 
         float dB = Mathf.Log10(volume) * 20;
         audioMixer.SetFloat("music", dB);
         PlayerPrefs.SetFloat("musicVolume", volume);
